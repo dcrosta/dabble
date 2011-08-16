de4928b3e9625279f3fb6dd875fbc3fa022ac374
Dan Crosta <dcrosta@gmail.com>
2011-08-16 15:21:04 -0400

    factor out file handling

--- dabble/backends/fs.py
+++ dabble/backends/fs.py
@@ -31,8 +31,49 @@ from os import SEEK_END
 from lockfile import FileLock
 import json
 
-# TODO: factor out scanning files for a matching record
-# TODO: factor out json dumping to remove excess spaces
+
+def find_lines(filename, **pattern):
+    """Find a line (JSON-formatted) in the given file where
+    all keys in `pattern` are present as keys in the line's
+    JSON, and where their values equal the corresponding
+    values in `pattern`. Additional keys in the line are
+    ignored. If no matching line is found, or if the
+    file does not exist, return None.
+    """
+    if exists(filename):
+        with file(filename, 'r') as fp:
+            for line in fp:
+                try:
+                    data = json.loads(line)
+                except:
+                    continue
+                matches = True
+                for key, value in pattern.iteritems():
+                    matches = matches and key in data and data[key] == value
+                if matches:
+                    yield data
+
+def find_line(filename, **pattern):
+    """Return the first line that would be found by
+    :func:`find_lines`.
+    """
+    for line in find_lines(filename, **pattern):
+        return line
+    return None
+
+lock = None
+def append_line(filename, **line):
+    """Safely (i.e. with locking) append a line to
+    the given file, serialized as JSON.
+    """
+    global lock
+
+    data = json.dumps(line, separators=(',', ':')) + '\n'
+    with lock:
+        with file(filename, 'a') as fp:
+            fp.seek(0, SEEK_END)
+            fp.write(data)
+
 
 class FSResultStorage(ResultStorage):
 
@@ -45,105 +86,49 @@ class FSResultStorage(ResultStorage):
             extension will be created.
           - `namespace`: the name prefix used to name collections
         """
+        global lock
+
         self.directory = abspath(directory)
 
         if not exists(self.directory):
             raise Exception('directory "%s" does not exist' % self.directory)
 
-        self.lock = FileLock(join(self.directory, 'lock.dabble'))
+        lock = FileLock(join(self.directory, 'lock.dabble'))
 
         self.tests_path = join(self.directory, 'tests.dabble')
         self.results_path = join(self.directory, 'results.dabble')
         self.alts_path = join(self.directory, 'alts.dabble')
 
     def save_test(self, test_name, alternatives):
-        if exists(self.tests_path):
-            fp = file(self.tests_path, 'r')
-            for line in fp:
-                data = json.loads(line)
-                if data['t'] == test_name:
-                    if data['a'] != alternatives:
-                        raise Exception(
-                            'test "%s" already exists with different alternatives' % test_name)
-                    return
+        existing = find_line(self.tests_path, t=test_name)
+        if existing and existing['a'] != alternatives:
+            raise Exception(
+                'test "%s" already exists with different alternatives' % test_name)
 
-        with self.lock:
-            # else test did not exist
-            fp = file(self.tests_path, 'a')
-            fp.seek(0, SEEK_END)
-            fp.write(json.dumps({'t': test_name, 'a': alternatives}))
-            fp.write('\n')
-            fp.close()
+        append_line(self.tests_path, t=test_name, a=alternatives)
 
     def record(self, identity, test_name, alternative, action, completed=False):
-        with self.lock:
-            fp = file(self.results_path, 'a')
-            fp.seek(0, SEEK_END)
-            fp.write(json.dumps({
-                'i': identity,
-                't': test_name,
-                'n': alternative,
-                'a': action,
-                'c': completed
-            }))
-            fp.write('\n')
-            fp.close()
+        append_line(self.results_path,
+                    i=identity, t=test_name, n=alternative, a=action, c=completed)
 
     def is_completed(self, identity, test_name, alternative):
-        if not exists(self.results_path):
-            return False
-
-        fp = file(self.results_path, 'r')
-        for line in fp:
-            data = json.loads(line)
-            if data['i'] == identity and data['t'] == test_name and \
-               data['n'] == alternative and data['c']:
-                return True
-
-        return False
+        completed = find_line(self.results_path, i=identity, t=test_name, n=alternative, c=True)
+        return completed is not None
 
     def set_alternative(self, identity, test_name, alternative):
-        if exists(self.alts_path):
-            fp = file(self.alts_path, 'r')
-            for line in fp:
-                data = json.loads(line)
-                if data['i'] == identity and data['t'] == test_name:
-                    raise Exception(
-                        'different alternative already set for identity %s' % identity)
+        existing = find_line(self.alts_path, i=identity, t=test_name)
+        if existing and existing['n'] != alternative:
+            raise Exception(
+                'different alternative already set for identity %s' % identity)
 
-        with self.lock:
-            fp = file(self.alts_path, 'a')
-            fp.seek(0, SEEK_END)
-            fp.write(json.dumps({
-                'i': identity,
-                't': test_name,
-                'n': alternative,
-            }))
-            fp.write('\n')
-            fp.close()
+        append_line(self.alts_path, i=identity, t=test_name, n=alternative)
 
     def get_alternative(self, identity, test_name):
-        if not exists(self.alts_path):
-            return None
-
-        fp = file(self.alts_path, 'r')
-        for line in fp:
-            data = json.loads(line)
-            if data['i'] == identity and data['t'] == test_name:
-                return data['n']
-
-        return None
+        existing = find_line(self.alts_path, i=identity, t=test_name) or {}
+        return existing.get('n')
 
     def ab_report(self, test_name, a, b):
-        test = None
-
-        fp = file(self.tests_path, 'r')
-        for line in fp:
-            data = json.loads(line)
-            if data['t'] == test_name:
-                test = data
-                break
-
+        test = find_line(self.tests_path, t=test_name)
         if test is None:
             raise Exception('unknown test "%s"' % test_name)
 
@@ -156,15 +141,12 @@ class FSResultStorage(ResultStorage):
             ]
         }
 
-        fp = file(self.results_path, 'r')
-        for line in fp:
-            data = json.loads(line)
-            if data['t'] == test_name:
-                result = out['results'][data['n']]
-                if data['a'] == a:
-                    result['attempted'].add(data['i'])
-                elif data['a'] == b and data['i'] in result['attempted']:
-                    result['completed'].add(data['i'])
+        for data in find_lines(self.results_path, t=test_name):
+            result = out['results'][data['n']]
+            if data['a'] == a:
+                result['attempted'].add(data['i'])
+            elif data['a'] == b and data['i'] in result['attempted']:
+                result['completed'].add(data['i'])
 
         for result in out['results']:
             result['attempted'] = len(result['attempted'])
