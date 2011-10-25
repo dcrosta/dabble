8e32a7c258b823fe93e56e788c7fdc585eb001d8
Dan Crosta <dcrosta@gmail.com>
2011-10-25 12:20:46 -0400

    bump to 0.2.2

--- dabble/__init__.py
+++ dabble/__init__.py
@@ -25,7 +25,7 @@
 
 __all__ = ('configure', 'IdentityProvider', 'ResultStorage', 'ABTest', 'ABParameter')
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 from datetime import datetime
 from hashlib import sha1
